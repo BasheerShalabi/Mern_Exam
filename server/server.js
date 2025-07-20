23e7228dbcd95320bdfcee0c93f89aa50a389c79
BasheerShalabi <shalabibasheer@gmail.com>
2025-07-20 20:42:23 +0300

    inital commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+require('dotenv').config()
+const PORT = process.env.PORT;
+
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require('./config/mongoose.config')
+
+app.get('/api', (req, res) => res.send('Hello from server!'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
